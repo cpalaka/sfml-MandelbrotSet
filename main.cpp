128b94e1b13e9d195fce4692851f202713cdad9e
Chaitanya <cpalaka@gmail.com>
2013-06-01 17:39:56 +0530

    all files

--- main.cpp
+++ main.cpp
@@ -25,51 +25,7 @@ int main()
 
 	float Re_factor = (max.real - min.real)/(WWIDTH-1);
 	float Im_factor = (max.imag - min.imag)/(WHEIGHT-1);
-/*
-	for(unsigned y=0; y<WHEIGHT; ++y)
-	{
-		color_no = -1;
-		float c_im = min.imag + y*Im_factor;
 
-		for(unsigned x=0; x<WWIDTH; ++x)
-		{
-			color_no = -1;
-			float c_re = min.real + x*Re_factor;
-
-			Complex c(c_re, c_im);
-			Complex c_const(c_re, c_im);
-			bool isInside = true;
-
-			for(unsigned n=0; n<MAXITER; ++n)
-			{
-				if(c.abs_squared() > 4.0) //if the absolute value of the complex number is greater than 2, then it escapes to infinity, and is not part of set
-				{
-					//Complex number is not part of the set
-					//or, it is outside the black inside area
-					color_no = n; //save the number of iterations it took to reach this point(infinity)
-					isInside = false;
-					break;
-				}
-				//z = z^2 + c
-				Complex c2 = c.cube();
-				c = c2.add(c_const);
-			}
-
-			if(isInside == true)
-			{
-				//Color pixels belonging to set black
-				image.setPixel(x, y, sf::Color::Black);
-			} 
-			if(color_no >= 0 && color_no < MAXITER/2-1)
-			{
-				image.setPixel(x, y, sf::Color((5*color_no)%255, 0, 0));
-			} else if (color_no >= MAXITER/2 && color_no < MAXITER)
-			{
-				image.setPixel(x, y, sf::Color((color_no)%255, (color_no)%255, (color_no)%255));
-			}
-		}
-	}
-*/
 	while(window.isOpen())
 	{
 		//Check events on window
